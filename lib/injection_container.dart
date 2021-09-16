fca06495ab147321704777e0b18bebf1e0ec81e8
oguzhan <oguzhan2142@hotmail.com>
2021-09-17 02:58:07 +0300

    create injection_container

---
+++ lib/injection_container.dart
@@ -0,0 +1,36 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:eksimsi_tdd_clean_architecture/core/platform/network_info.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/data/datasources/agenda_repository_remote_data_source.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/data/repositories/agenda_repository_impl.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/domain/repositories/agenda_repository.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/domain/usecases/get_agenda_entries_page.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/domain/usecases/get_agenda_headers.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/presentation/blocs/entry_page_bloc/entry_page_bloc.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/presentation/blocs/headers_bloc/headers_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+void init() {
+  // Features
+
+  sl.registerFactory(() => HeadersBloc(getAgendaHeaders: sl()));
+  sl.registerFactory(() => EntryPageBloc(getAgendaEntriesPage: sl()));
+
+  sl.registerLazySingleton(() => GetAgendaHeaders(sl()));
+  sl.registerLazySingleton(() => GetAgendaEntriesPage(sl()));
+
+  sl.registerLazySingleton<AgendaRepository>(() => AgendaRepositoryImp(
+      agendaRepositoryRemoteDataSource: sl(), networkInfo: sl()));
+
+  sl.registerLazySingleton<AgendaRepositoryRemoteDataSource>(
+      () => AgendaRepositoryRemoteDataSourceImpl(client: sl()));
+
+  // Core
+
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  // External
+
+  sl.registerLazySingleton(() => Connectivity());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/presentation/blocs/entry_page_bloc/entry_page_bloc.dart';
+import 'package:eksimsi_tdd_clean_architecture/features/agenda/presentation/blocs/headers_bloc/headers_bloc.dart';
+
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'injection_container.dart';
+
+void main() {
+  init();
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<HeadersBloc>(
+        create: (context) => sl<HeadersBloc>(),
+      ),
+      BlocProvider<EntryPageBloc>(
+        create: (context) => sl<EntryPageBloc>(),
+      )
+    ],
+    child: MyApp(),
+  ));
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
